46dce7e0cb072f7a977434c9d0084ca0872c6241
jchancellor-ms <jchancellor@microsoft.com>
2024-01-05 20:10:31 -0800

    fixed script references

--- modules/create_test_domain_controller/templates/dc_windows_dsc.ps1
+++ modules/create_test_domain_controller/templates/dc_windows_dsc.ps1
@@ -25,7 +25,7 @@ lcmConfig -InstanceName localhost -OutputPath .\lcmConfig
 Set-DscLocalConfigurationManager -Path .\lcmConfig -Verbose
 
 [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ("$env:ACTIVEDIRECTORYNETBIOS\$env:ADMINUSERNAME", (ConvertTo-SecureString "$env:ADMINPASSWORD" -AsPlainText -Force))
-$ldapUserPassword = (ConvertTo-SecureString "$env:LDAPUSERPASSWORD" -AsPlainText -Force)
+[pscredential]$ldapUserPassword = New-Object System.Management.Automation.PSCredential("$env:ACTIVEDIRECTORYNETBIOS\$env:LDAPUSER", (ConvertTo-SecureString "$env:LDAPUSERPASSWORD" -AsPlainText -Force))
 
 Configuration dc {
    
