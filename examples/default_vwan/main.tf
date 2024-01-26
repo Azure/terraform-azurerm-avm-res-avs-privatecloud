f8b8d85653d8403ee277123fcd7961174931c34f
jchancellor-ms <jchancellor@microsoft.com>
2024-01-25 18:46:08 -0800

    fixed dns destroy issue

--- examples/default_vwan/main.tf
+++ examples/default_vwan/main.tf
@@ -1,17 +1,17 @@
 terraform {
-  required_version = "~>1.6.0"
+  required_version = "~>1.6"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.7.0"
+      version = "~> 3.74"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.0"
+      version = "~> 3.5"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "~> 1.9.0"
+      version = "~> 1.9"
     }
   }
 }
@@ -36,7 +36,7 @@ locals {
 
 module "naming" {
   source  = "Azure/naming/azurerm"
-  version = "= 0.3.0"
+  version = "= 0.4.0"
 }
 
 module "regions" {
--- main.networking.dns.tf
+++ main.networking.dns.tf
@@ -10,17 +10,10 @@ resource "azapi_resource" "dns_forwarder_zones" {
       displayName  = each.value.display_name
       dnsServerIps = each.value.dns_server_ips
       domain       = each.value.domain_names
-      revision     = each.value.revision
+      #revision     = each.value.revision
       sourceIp     = each.value.source_ip
     }
   })
-
-  #adding lifecycle block to handle replacement issue with parent_id
-  #lifecycle {
-  #  ignore_changes = [
-  ##    parent_id
-  #  ]
-  #}
 
   depends_on = [
     azapi_resource.this_private_cloud,
@@ -71,32 +64,70 @@ data "azapi_resource_action" "avs_dns" {
   ]
 }
 
-#modify the existing DNS Service (only allowing addition of FQDN zones currently)
-#resource "azapi_update_resource" "dns_service" {
-
-
-
 resource "azapi_resource_action" "dns_service" {
-  #check to see if the fqdn_zones match.  If they don't, update the zones. The API doesn't handle this nicely if they match and the update is null
+  count = (length(keys(var.dns_forwarder_zones))) == 0 ? 0 : 1
 
   type        = "Microsoft.AVS/privateClouds/workloadNetworks/dnsServices@2022-05-01"
   resource_id = "${azapi_resource.this_private_cloud.id}/workloadNetworks/default/dnsServices/dns-forwarder"
-  #if zone information is defined populate the properties. Otherwise send an empty body
-  #body = try(jsondecode(data.azapi_resource_action.avs_dns.output).value[0].properties.fqdnZones, []) != ([for key, zone in var.dns_forwarder_zones : key if zone.add_to_default_dns_service]) ? jsonencode({
+  #if zone information defined populate the properties
   body = jsonencode({
     properties = {
       defaultDnsZone = jsondecode(data.azapi_resource_action.avs_dns.output).value[0].properties.defaultDnsZone
       displayName    = jsondecode(data.azapi_resource_action.avs_dns.output).value[0].properties.displayName
       dnsServiceIp   = jsondecode(data.azapi_resource_action.avs_dns.output).value[0].properties.dnsServiceIp
-      fqdnZones      = [for key, zone in var.dns_forwarder_zones : key if zone.add_to_default_dns_service]
+      fqdnZones      = try([for key, zone in var.dns_forwarder_zones : key if zone.add_to_default_dns_service], [])
       logLevel       = jsondecode(data.azapi_resource_action.avs_dns.output).value[0].properties.logLevel
-      revision       = 0
+      #revision       = 0
     }
   })
-  #}) : "{}"
-  #if zone information is defined, use the PATCH method.  Otherwise perform a GET operation to just do a read
-  #method = try(jsondecode(data.azapi_resource_action.avs_dns.output).value[0].properties.fqdnZones, []) != ([for key, zone in var.dns_forwarder_zones : key if zone.add_to_default_dns_service]) ? "PATCH" : "GET"
+
   method = "PATCH"
+  when   = "apply"
+
+
+  depends_on = [
+    azapi_resource.this_private_cloud,
+    azapi_resource.clusters,
+    azurerm_role_assignment.this_private_cloud,
+    azurerm_monitor_diagnostic_setting.this_private_cloud_diags,
+    azapi_update_resource.managed_identity,
+    azapi_update_resource.customer_managed_key,
+    azapi_resource.hcx_addon,
+    azapi_resource.hcx_keys,
+    azapi_resource.srm_addon,
+    azapi_resource.vr_addon,
+    azurerm_express_route_connection.avs_private_cloud_connection,
+    azurerm_virtual_network_gateway_connection.this,
+    azapi_resource.globalreach_connections,
+    azapi_resource.dns_forwarder_zones
+  ]
+
+  timeouts {
+    create = "4h"
+    delete = "4h"
+    update = "4h"
+  }
+}
+
+resource "azapi_resource_action" "dns_service_destroy_non_empty_start" {
+  count = length(keys(var.dns_forwarder_zones)) > 0 ? 1 : 0
+
+  type        = "Microsoft.AVS/privateClouds/workloadNetworks/dnsServices@2022-05-01"
+  resource_id = "${azapi_resource.this_private_cloud.id}/workloadNetworks/default/dnsServices/dns-forwarder"
+  #if zone information defined populate the properties
+  body = jsonencode({
+    properties = {
+      defaultDnsZone = jsondecode(data.azapi_resource_action.avs_dns.output).value[0].properties.defaultDnsZone
+      displayName    = jsondecode(data.azapi_resource_action.avs_dns.output).value[0].properties.displayName
+      dnsServiceIp   = jsondecode(data.azapi_resource_action.avs_dns.output).value[0].properties.dnsServiceIp
+      fqdnZones      = []
+      logLevel       = jsondecode(data.azapi_resource_action.avs_dns.output).value[0].properties.logLevel
+      #revision       = 0
+    }
+  })
+
+  method = "PATCH"
+  when   = "destroy"
 
 
   depends_on = [
