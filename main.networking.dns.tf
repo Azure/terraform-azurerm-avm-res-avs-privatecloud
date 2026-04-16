ced3b090f1d61c62c691cc6e829fbd154228195a
Jon Chancellor <jchancellor@microsoft.com>
2026-04-16 10:52:26 -0700

    default DNS method change

--- main.networking.dns.tf
+++ main.networking.dns.tf
@@ -185,7 +185,7 @@ data "azapi_resource_action" "avs_default_dns" {
     azapi_resource.this_private_cloud
   ]
 }
-
+/*
 resource "azapi_update_resource" "dns_default_service_ips" {
   for_each = length(var.default_dns_ips) > 0 ? { "default" : var.default_dns_ips } : {}
 
@@ -221,5 +221,91 @@ resource "azapi_update_resource" "dns_default_service_ips" {
     azapi_resource.avs_interconnect,
     azapi_resource.dns_forwarder_zones,
     data.azapi_resource_action.avs_default_dns
+  ]
+}
+*/
+
+### Testing
+resource "azapi_resource_action" "dns_default_zone" {
+  for_each = length(var.default_dns_ips) > 0 ? { "default" : var.default_dns_ips } : {}
+
+  method      = "PATCH"
+  resource_id = data.azapi_resource_action.avs_default_dns["default"].output.value[0].id
+  type        = "Microsoft.AVS/privateClouds/workloadNetworks/dnsZones@2023-09-01"
+  body = {
+    properties = {
+      displayName  = data.azapi_resource_action.avs_default_dns["default"].output.value[0].properties.displayName
+      dnsServerIps = var.default_dns_ips
+      domain       = []
+      dnsServices  = 1
+    }
+  }
+  when = "apply"
+
+  timeouts {
+    create = "4h"
+    delete = "4h"
+  }
+
+  depends_on = [
+    azapi_resource.this_private_cloud,
+    azapi_resource.clusters,
+    azurerm_role_assignment.this_private_cloud,
+    azurerm_monitor_diagnostic_setting.this_private_cloud_diags,
+    azapi_update_resource.customer_managed_key,
+    azapi_resource.hcx_addon,
+    azapi_resource.hcx_keys,
+    azapi_resource.srm_addon,
+    azapi_resource.vr_addon,
+    azurerm_express_route_connection.avs_private_cloud_connection,
+    azurerm_express_route_connection.avs_private_cloud_connection_additional,
+    azapi_resource.avs_private_cloud_expressroute_vnet_gateway_connection,
+    azapi_resource.avs_private_cloud_expressroute_vnet_gateway_connection_additional,
+    azapi_resource.globalreach_connections,
+    azapi_resource.avs_interconnect,
+    azapi_resource.dns_forwarder_zones,
+    data.azapi_resource_action.avs_default_dns
+  ]
+}
+
+
+resource "azapi_resource_action" "dns_zone_destroy_non_empty_start" {
+  for_each = length(var.default_dns_ips) > 0 ? { "default" : var.default_dns_ips } : {}
+
+  method      = "PATCH"
+  resource_id = data.azapi_resource_action.avs_default_dns["default"].output.value[0].id
+  type        = "Microsoft.AVS/privateClouds/workloadNetworks/dnsZones@2023-09-01"
+  body = {
+    properties = {
+      displayName  = data.azapi_resource_action.avs_default_dns["default"].output.value[0].properties.displayName
+      dnsServerIps = data.azapi_resource_action.avs_default_dns["default"].output.value[0].properties.dnsServerIps
+      domain       = []
+      dnsServices  = 1
+    }
+  }
+  when = "destroy"
+
+  timeouts {
+    create = "4h"
+    delete = "4h"
+  }
+
+  depends_on = [
+    azapi_resource.this_private_cloud,
+    azapi_resource.clusters,
+    azurerm_role_assignment.this_private_cloud,
+    azurerm_monitor_diagnostic_setting.this_private_cloud_diags,
+    azapi_update_resource.customer_managed_key,
+    azapi_resource.hcx_addon,
+    azapi_resource.hcx_keys,
+    azapi_resource.srm_addon,
+    azapi_resource.vr_addon,
+    azurerm_express_route_connection.avs_private_cloud_connection,
+    azurerm_express_route_connection.avs_private_cloud_connection_additional,
+    azapi_resource.avs_private_cloud_expressroute_vnet_gateway_connection,
+    azapi_resource.avs_private_cloud_expressroute_vnet_gateway_connection_additional,
+    azapi_resource.globalreach_connections,
+    azapi_resource.avs_interconnect,
+    azapi_resource.dns_forwarder_zones
   ]
 }
